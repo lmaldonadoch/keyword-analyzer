15cbecd791ee49d4c6b14f33bdcac4fbd59c6c3c
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-10 16:19:28 -0500

    Made login the home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
       <router-link to="/login">Log in</router-link> |
       <router-link to="/register">Register</router-link> |
       <router-link to="/keywordsearch">Main</router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import KeyWordSearch from '../views/KeyWordSearch.vue';
@@ -12,8 +11,8 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'login',
+    component: Login,
   },
   {
     path: '/login',
